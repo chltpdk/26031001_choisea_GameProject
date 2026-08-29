45c68ba9ba6de9a5dc34c86fce4e2e9f927bec2b
chltpdk <chltpdk1029@gmail.com>
2026-08-29 20:57:03 +0900

    프로젝트 자기이력서

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("┌────────────────────────────────────────────────────────┐");
+            Console.WriteLine("         ======================================");
+            Console.WriteLine("                 안녕하세요! 이력서입니다     ");
+            Console.WriteLine("         ======================================");
+            Console.WriteLine();  //공백 추가
+            Console.WriteLine("         이    름 : 최세아");
+            Console.WriteLine("         생년월일 : 2006년 10월 10일");
+            Console.WriteLine("         전화번호 : 010-8011-2798");
+            Console.WriteLine("         이 메 일 : chltpdk06@naver.com");
+            Console.WriteLine();
+            Console.WriteLine("         ======================================");
+
+            //학력사항추가
+
+            Console.WriteLine();
+            Console.WriteLine("                      [학력사항]");
+            Console.WriteLine();
+            Console.WriteLine("       ------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("        2019.3 ~ 2022.2  인성여자중학교(졸업)");
+            Console.WriteLine("        2022.3 ~ 2025.2  인천여자상업고등학교(졸업)");
+            Console.WriteLine("        전공 : iT크리에이터과");
+            Console.WriteLine("        2026.3 ~ 2029.2  한국iT직업전문학교(졸업예정)");
+            Console.WriteLine("        전공 : 게임기획학과");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("                        [TMI]");
+            Console.WriteLine();
+            Console.WriteLine("       ------------------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("        취      미 : 게임, 영화, 독서(조금), 꾸미기");
+            Console.WriteLine("        M  B  T  I : INFP");
+            Console.WriteLine("        좋아하는 것: 우사기, 해산물, 수탉");
+            Console.WriteLine("        반 려 동 물: 최세봄 (강아지)");
+            Console.WriteLine();
+            Console.WriteLine("└────────────────────────────────────────────────────────┘");
+        }
+    }
+}
