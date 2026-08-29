43f74dfee1101cf0d6744b0fa8ba48f1b53f25df
chltpdk <chltpdk1029@gmail.com>
2026-08-29 20:59:20 +0900

    프로젝트 자기이력서

--- 26301001_최세아_객체지향프로그래밍_자기이력서/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ 26301001_최세아_객체지향프로그래밍_자기이력서/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1e4d70e81df3851065e6f7378d550a92d8a8c4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c68ba9ba6de9a5dc34c86fce4e2e9f927bec2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
